31921aa8980467d97cddcdd4a8414c753b33ace5
Tennessee Février <tfevrier@apizr.fr>
2021-07-23 17:00:58 +0200

    WIP: fix MindmapView import

---
+++ webpack.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  resolve: {
+    fallback: {
+      os: false,
+      emitter: false,
+      cryto: false,
+      http: false,
+      https: false,
+      zlib: false,
+      path: false,
+      fs: false,
+    },
+  },
+};
