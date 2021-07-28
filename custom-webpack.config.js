d2b1a1afdcfcf0cb6c160b499e93146f7f23cc78
Tennessee Février <tfevrier@apizr.fr>
2021-07-28 16:38:42 +0200

    chore: clean webpack config

--- custom-webpack.config.js
+++ custom-webpack.config.js
@@ -4,11 +4,6 @@ module.exports = {
   resolve: {
     fallback: {
       os: require.resolve("os-browserify/browser"),
-      crypto: require.resolve("crypto-browserify"),
-      path: require.resolve("path-browserify"),
-      http: require.resolve("stream-http"),
-      https: require.resolve("https-browserify"),
-      zlib: require.resolve("browserify-zlib"),
     },
   },
 };
