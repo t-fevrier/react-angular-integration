2bdf6074caae8324273bfb9ea09690fc9e7f8b0a
Julien Giovaresco <dev@giovaresco.fr>
2021-08-11 17:30:14 +0200

    feat: add mindmap-wrapper

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { MindmapComponent } from './react-components/mindmap.component';
+import { MindmapWrapperComponent } from './mindmap-wrapper';
 
 @NgModule({
-  declarations: [AppComponent, MindmapComponent],
+  declarations: [AppComponent, MindmapComponent, MindmapWrapperComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
